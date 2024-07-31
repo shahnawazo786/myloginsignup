836546871080deaf01d628834da7b8df8037ea05
Shahnawaz Hussain <142620985+shahnawazo786@users.noreply.github.com>
2024-07-31 12:57:01 +0530

    Update signup.js

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,30 +1,29 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { handleError, handleSuccess } from '../utils';
 
 function Signup() {
-
     const [signupInfo, setSignupInfo] = useState({
         name: '',
         email: '',
         password: ''
-    })
+    });
 
     const navigate = useNavigate();
+
     const handleChange = (e) => {
         const { name, value } = e.target;
-        console.log(name, value);
         const copySignupInfo = { ...signupInfo };
         copySignupInfo[name] = value;
         setSignupInfo(copySignupInfo);
-    }
+    };
 
     const handleSignup = async (e) => {
         e.preventDefault();
         const { name, email, password } = signupInfo;
         if (!name || !email || !password) {
-            return handleError('name, email and password are required')
+            return handleError('Name, email, and password are required');
         }
         try {
             const url = `https://myloginsignup-api.vercel.app/auth/signup`;
@@ -39,20 +38,21 @@ function Signup() {
             const { success, message, error } = result;
             if (success) {
                 handleSuccess(message);
+                setSignupInfo({ name: '', email: '', password: '' }); // Clear form state
                 setTimeout(() => {
-                    navigate('/login')
-                }, 1000)
+                    navigate('/login');
+                }, 1000);
             } else if (error) {
-                const details = error?.details[0].message;
+                const details = error.details ? error.details[0].message : error.message;
                 handleError(details);
-            } else if (!success) {
+            } else {
                 handleError(message);
             }
-            console.log(result);
         } catch (err) {
-            handleError(err);
+            handleError(err.message);
         }
-    }
+    };
+
     return (
         <div className='container'>
             <h1>Signup</h1>
@@ -89,13 +89,13 @@ function Signup() {
                     />
                 </div>
                 <button type='submit'>Signup</button>
-                <span>Already have an account ?
-                    <Link to="/login">Login</Link>
+                <span>Already have an account?
+                    <Link to="/login"> Login</Link>
                 </span>
             </form>
             <ToastContainer />
         </div>
-    )
+    );
 }
 
-export default Signup
+export default Signup;
